e5ca0c938f243fe4e42225141cce8ce02d0b4c92
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2022-08-09 19:48:54 -0400

    noficiation

--- site/src/components/ContactForm/index.tsx
+++ site/src/components/ContactForm/index.tsx
@@ -1,6 +1,6 @@
 import 'antd/dist/antd.css';
 import React from "react";
-import { Row, Col, Button, Form, Input } from "antd"
+import { Row, Col, Button, Form, Input, notification } from "antd"
 import { UserOutlined, MailOutlined } from "@ant-design/icons"
 
 const { TextArea } = Input
@@ -10,6 +10,16 @@ function encode(data: any) {
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
         .join("&")
 }
+
+const openNotification = () => {
+    notification.open({
+        message: "Submitted!",
+        description: "Thank you for your submission! We will get back to you shortly.",
+        onClick: () => {
+            console.log("Notification Clicked");
+        },
+    });
+};
 
 const ContactForm = () => {
     const formName = "contact"
@@ -28,11 +38,12 @@ const ContactForm = () => {
             })
         })
             .then(() => showSuccess())
-            .catch(error => showError(error))
+            .catch(error => showError(error));
     }
 
     const showSuccess = () => {
-        console.log("Form successfully submitted")
+        openNotification();
+        console.log("Form successfully submitted");
     }
 
     const showError = (error: string) => {
@@ -40,88 +51,89 @@ const ContactForm = () => {
     }
 
     return (
-        <Row
-            justify="space-around"
-        >
-            <Col
-                xs={22}
-                sm={18}
-                md={16}
-                lg={8}
+        <div>
+            <Row
+                justify="space-around"
             >
-                {/* 
-                    Defines how form is set up for Netlify form bot 
-                */}
-                <form
-                    name={formName}
-                    data-netlify="true"
-                    data-netlify-honeypot="bot-field"
-                    hidden
-                >
-                    <input type="text" name="name" />
-                    <input type="email" name="email" />
-                    <textarea name="message"></textarea>
-                </form>
-                <Form
-                    name="contactForm"
-                    method="post"
-                    onFinish={handleSubmit}
-                    layout="vertical"
-                >
-                    {/*
-                        Hidden field that Netlify uses
+                <Col
+                    xs={22}
+                    sm={18}
+                    md={16}
+                    lg={8}
+                    >
+                    {/* 
+                        Defines how form is set up for Netlify form bot 
                     */}
-                    <Form.Item
-                        label="Don't fill this out"
-                        className={"hidden"}
-                        style={{ display: "none" }}
-                        name="bot-field"
-                    >
-                        <Input type={"hidden"} />
-                    </Form.Item>
+                    <form
+                        name={formName}
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field"
+                        hidden
+                        >
+                        <input type="text" name="name" />
+                        <input type="email" name="email" />
+                        <textarea name="message"></textarea>
+                    </form>
+                    <Form
+                        name="contactForm"
+                        method="post"
+                        onFinish={handleSubmit}
+                        layout="vertical"
+                        >
+                        {/*
+                            Hidden field that Netlify uses
+                        */}
+                        <Form.Item
+                            label="Don't fill this out"
+                            className={"hidden"}
+                            style={{ display: "none" }}
+                            name="bot-field"
+                            >
+                            <Input type={"hidden"} />
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Name"
-                        rules={[{ required: true, message: "Please enter your name." }]}
-                        name="name"
-                    >
-                        <Input 
-                            placeholder="Name"
-                            prefix={<UserOutlined className="contact-form-item-icon" />}
-                        />
-                    </Form.Item>
+                        <Form.Item
+                            label="Name"
+                            rules={[{ required: true, message: "Please enter your name." }]}
+                            name="name"
+                            >
+                            <Input 
+                                placeholder="Name"
+                                prefix={<UserOutlined className="contact-form-item-icon" />}
+                                />
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Email"
-                        rules={[{ required: true, message: "Please enter your email" }]}
-                        name="email"
-                    >
-                        <Input 
-                            placeholder="Email"
-                            prefix={<MailOutlined className="contact-form-item-icon" />}
-                        />
-                    </Form.Item>
+                        <Form.Item
+                            label="Email"
+                            rules={[{ required: true, message: "Please enter your email" }]}
+                            name="email"
+                            >
+                            <Input 
+                                placeholder="Email"
+                                prefix={<MailOutlined className="contact-form-item-icon" />}
+                                />
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Message"
-                        rules={[{ required: true, message: "Please enter your message" }]}
-                        name="message"
-                    >
-                        <TextArea 
-                            placeholder="Message"
-                            rows={5}    
-                        />
-                    </Form.Item>
+                        <Form.Item
+                            label="Message"
+                            rules={[{ required: true, message: "Please enter your message" }]}
+                            name="message"
+                            >
+                            <TextArea 
+                                placeholder="Message"
+                                rows={5}    
+                                />
+                        </Form.Item>
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" disabled={false}>
-                            Submit
-                        </Button>
-                    </Form.Item>
-
-                </Form>
-            </Col>
-        </Row>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" disabled={false}>
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
