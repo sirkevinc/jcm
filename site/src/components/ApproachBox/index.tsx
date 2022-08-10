5d5ba14d41b48dce53c525147a04118fc960fb85
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2022-08-09 21:05:58 -0400

    Testing some things. Maybe it work maybe it doesn't

--- site/src/components/ApproachBox/index.tsx
+++ site/src/components/ApproachBox/index.tsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const ApproachBox = () => {
     return (
-        <div>
-            Box
+        <div className="approach__container">
+            <div className="approach__upper">
+                <h3>Our Mindset</h3>
+                <p>Your business is our business, meaning we’re not just hired hands — we’re dedicated and experienced extensions of your team who care for your business as if it were our own.</p>
+            </div>
+            <div className="approach__bottom">
+                <div className="approach__col">
+                    <h4>What We Stand For</h4>
+                    <p>We are committed to getting you where you need to go and we’ll be with you every step of the way.</p>
+                </div>
+                <div className="approach__col">
+                    <h4>What We Value</h4>
+                    <p>We believe success comes from working together to do good work and serve those around us.</p>
+                </div>
+                <div className="approach__col">
+                    <h4>Why We're Here</h4>
+                    <p>We’re deeply committed to everything we do and it shows in the events we host, causes we support and awards we win.</p>
+                </div>
+            </div>
         </div>
     )
 }
