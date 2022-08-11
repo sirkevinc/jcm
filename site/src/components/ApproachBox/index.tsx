dedb767240b7820c5cdbba4d6e98aca6f21a130c
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2022-08-11 15:36:03 -0400

    Some word changes

--- site/src/components/ApproachBox/index.tsx
+++ site/src/components/ApproachBox/index.tsx
@@ -4,21 +4,21 @@ const ApproachBox = () => {
     return (
         <div className="approach__container">
             <div className="approach__upper">
-                <h3>Our Mindset</h3>
-                <p>Your business is our business, meaning we’re not just hired hands — we’re dedicated and experienced extensions of your team who care for your business as if it were our own.</p>
+                <h3>Our Approach</h3>
+                <p>We treat your business as our business — we’re act as dedicated and experienced extensions of your team who care for your business as if it were our own.</p>
             </div>
             <div className="approach__bottom">
                 <div className="approach__col">
                     <h4>What We Stand For</h4>
-                    <p>We are committed to getting you where you need to go and we’ll be with you every step of the way.</p>
+                    <p>We are committed to helping you reach your goals and we’ll be with you every step of the way.</p>
                 </div>
                 <div className="approach__col">
                     <h4>What We Value</h4>
-                    <p>We believe success comes from working together to do good work and serve those around us.</p>
+                    <p>We believe in working together to do good work and serve those around us.</p>
                 </div>
                 <div className="approach__col">
                     <h4>Why We're Here</h4>
-                    <p>We’re deeply committed to everything we do and it shows in the events we host, causes we support and awards we win.</p>
+                    <p>We’re deeply committed to everything we do and it shows in the quality of our work.</p>
                 </div>
             </div>
         </div>
